acf8b0ae82a5a751d4b2d13d0ef19a743203e161
jgaynor17 <jgaynor2@sheffield.ac.uk>
2023-08-15 16:57:12 +0100

    Improved CSS

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -3,27 +3,30 @@ import Error from '../error/error';
 
 const Feed = ({ posts, error }) => {
     return (
-        <div id="post-container">
-            <div>
-                {error && <Error error={error} />}
-            </div>
-            {posts.map(post => (
-                <div key={post.id} className='post'>
-                    <a href={`https://www.reddit.com${post.permalink}`} target='_blank'><h2 id='post-title'>{post.title}</h2></a>
-                    {post.url_overridden_by_dest && post.post_hint === "image" && !post.is_video && !post.media_metadata && (
-                        <img className='post-media' src={post.url_overridden_by_dest} alt={post.title} />
-                    )}
-                    {post.is_video && post.media.reddit_video.fallback_url &&  (
-                        <video controls
-                            title={`Embedded Media ${post.index}`}
-                            src={post.media.reddit_video.fallback_url}
-                            allowFullScreen
-                            className="post-media"
-                        />
-                    )}
+        <div id='post-box'>
+            <div id="post-container">
+                <div>
+                    {error && <Error error={error} />}
                 </div>
-            ))}
+                {posts.map(post => (
+                    <div key={post.id} className='post'>
+                        <a href={`https://www.reddit.com${post.permalink}`} target='_blank'><h2 id='post-title'>{post.title}</h2></a>
+                        {post.url_overridden_by_dest && post.post_hint === "image" && !post.is_video && !post.media_metadata && (
+                            <img className='post-media' src={post.url_overridden_by_dest} alt={post.title} />
+                        )}
+                        {post.is_video && post.media.reddit_video.fallback_url &&  (
+                            <video controls
+                                title={`Embedded Media ${post.index}`}
+                                src={post.media.reddit_video.fallback_url}
+                                allowFullScreen
+                                className="post-media"
+                            />
+                        )}
+                    </div>
+                ))}
+            </div>            
         </div>
+
     )
 }
 
